7627992aab398474dd5baae902c5a1f85e530abd
VedantWankhade <vedantwankhade17@gmail.com>
2021-01-18 00:13:37 +0530

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
-  /*   return createElement("div", {}, [
-    createElement("h1", {}, "Adopt Me"),
-    createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanesse",
-    }),
-    createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot",
-    }),
-    createElement(Pet, { name: "Doink", animal: "Cat", breed: "Stray" }),
-  ]); */
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Peep" animal="Birb" breed="Parrot" />
-      <Pet name="Doink" animal="Cat" breed="Stray" />
+      <SearchParams />
     </div>
   );
 };
